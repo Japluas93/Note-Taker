b19c1e73514201d1d42bd29685c956f5dccc7f56
Julian Pluas <julianpluas@gmail>
2021-02-22 23:31:44 -0800

    Added comments to explain the code in my server js file

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
+// Loads my express module
 const express = require("express");
-
+// Imports my htmlroutes file
 const htmlRoutes = require("./routes/htmlroutes");
-
+// Imports my apiroutes file
 const apiRoutes = require("./routes/apiroutes");
-
+// Creates my express application
 const app = express();
+// App registers a middleware function that parses JSON
 app.use(express.json());
+// App registers a middleware function that parses urlencoded bodies
 app.use(express.urlencoded({ extended: true }));
+// App registers a middleware function that serves images, CSS files, and JavaScript files in a directory named public
 app.use(express.static("public"));
-
+// App registers the required api route
 app.use(apiRoutes);
-
+// App registers the requires html route
 app.use(htmlRoutes);
-
+// App listens on port 3000 for connections
 app.listen(3000, function () {
   console.log("Server is running");
 });
