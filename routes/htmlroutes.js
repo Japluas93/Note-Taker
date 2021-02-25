764faf5bd9078d3adcf7f42e9378756fb1c7c2d4
Julian Pluas <julianpluas@gmail>
2021-02-24 22:15:03 -0800

    Added a comment in my html routes file that explains what my get methods are returning. Also commented where my html routes are being exported

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,10 +1,12 @@
 const htmlRouter = require("express").Router();
 const path = require("path");
+// This route returns the `notes.html` file.
 htmlRouter.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
+// This route returns the `index.html` file
 htmlRouter.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
-
+// htmlRouter is exported
 module.exports = htmlRouter;
