1278fbd66082bafc1f61c2dc916b5970a72492c2
Julian Pluas <julianpluas@gmail>
2021-02-24 21:53:26 -0800

    Changed the name of my variable

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,4 +1,4 @@
-const router = require("express").Router();
+const htmlRouter = require("express").Router();
 const path = require("path");
 router.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
