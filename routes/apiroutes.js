8839b234a3dd55ebef4604825988072a75e2dacf
Julian Pluas <julianpluas@gmail>
2021-02-24 21:36:13 -0800

    Changed the name of my variable in the api routes file

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,11 +1,10 @@
-var router = require("express").Router();
+const apiRouter = require("express").Router();
 
-router.get("*", function (req, res) {
+apiRouter.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-//fs.readfile
-router.get("/api/notes", function (req, res) {
+apiRouter.get("/api/notes", function (req, res) {
   res.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
     if (err) {
       console.log("Error");
@@ -15,9 +14,30 @@ router.get("/api/notes", function (req, res) {
   });
   res.json(notes);
 });
-//fs.writefile for post and delete methods
+
+apiRouter.post("/api/notes", function (req, res) {
+  let note = {
+    title: req.body.title,
+    text: req.body.text,
+  };
+  notes.push(note);
+  res.writeFile(
+    path.join(__dirname, "../db/db.json"),
+    JSON.stringify(notes),
+    (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      // notes.push({title:"my test", text:"my text"})
+      res.send(notes);
+    }
+  );
+});
+// fs.readfile
+//fs.writefile for post adnd delete methods
 // json parse and stringify
 // req.body
 // req.params
 
-module.exports = router;
+module.exports = apiRouter;
