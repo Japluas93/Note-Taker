4f1b97b66b12ce178ccaa9345949154d3153b807
Julian Pluas <julianpluas@gmail>
2021-03-04 21:54:55 -0800

    Fixed a syntax error with var port

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(apiRoutes);
 // App registers the required html route
 app.use(htmlRoutes);
 // Variable that ensures that my web server is starting with a dynamic port.
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 // App listens on port 3000 for connections
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("Server is running");
 });
